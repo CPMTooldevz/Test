387576f6f24900058da4d4fcb0768a645aa75c18
CPMTooldevz <telmnanotheremail@gmail.com>
2025-02-23 01:40:51 +0800

    Update cpmtooldev91.py

--- cpmtooldev91.py
+++ cpmtooldev91.py
@@ -1,8 +1,8 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://telmunnshop.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://testers.squareweb.app/api"
 
-class CPMTooldev:
+class CPMTooldevz:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
