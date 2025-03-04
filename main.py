b35fb29bf9f1a0d9858bc38c5913f28d323967a6
CPMTooldevz <telmnanotheremail@gmail.com>
2025-03-04 16:20:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -626,7 +626,7 @@ if __name__ == "__main__":
                     print(Colorate.Horizontal(Colors.rainbow, 'PLEASE TRY AGAIN'))
                     sleep(2)
                     continue
-             elif service == 27:
+            elif service == 27:
                 console.print("[bold yellow][!] Note[/bold yellow]: original tire can not be restored!.")
                 console.print("[bold cyan][!] Enter Car Details.[/bold cyan]")
                 car_id = IntPrompt.ask("[bold][?] Car Id[/bold]")
