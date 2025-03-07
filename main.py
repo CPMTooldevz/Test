3044ce4519422a3dd33f256f66096a2f04b6c721
CPMTooldevz <telmnanotheremail@gmail.com>
2025-03-07 20:29:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ def banner(console):
     console.print("==================================================")
     console.print("[bold yellow]! Note[/bold yellow]: Logout from CPM before using this tool !.", end="\n\n")
 
-def load_player_data(cpm):
-    data = cpm.get_data()
+def get_player_data(cpm):
+    data = cpm.get_player_data()
     console.print("[bold][red]========[/red][ PLAYER DETAILS ][red]========[/red][/bold]")
     console.print(f"[bold green] Нэр   [/bold green]: {data.get('Name') if 'Name' in data else 'UNDEFINED'}.")
     console.print(f"[bold green] ID    [/bold green]: {data.get('localID') if 'localID' in data else 'UNDEFINED'}.")
     console.print(f"[bold green] Мөнгө [/bold green]: {data.get('money') if 'money' in data else 'UNDEFINED'}.")
     console.print(f"[bold green] Зоос  [/bold green]: {data.get('coin') if 'coin' in data else 'UNDEFINED'}.")
 
-def load_key_data(cpm):
+def get_key_data(cpm):
     data = cpm.get_key_data()
     console.print("[bold][red]========[/red][ ACCESS KEY DETAILS ][red]========[/red][/bold]")
     console.print(f"[bold green] Зөвшөөрлийн түлхүүр[/bold green]: {data.get('access_key') }.")
